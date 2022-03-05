b5cdafdea27ec47d6509bf385cb317d75c3d1d8b
Rodasistemas <rodasistemas@gmail.com>
2022-03-05 07:14:55 -0300

    feat(page404): style and animation for 404

--- src/components/Pages/PageNotFound/index.tsx
+++ src/components/Pages/PageNotFound/index.tsx
@@ -1,11 +1,36 @@
 import { Container } from 'components/Layout'
+import { Link } from 'react-router-dom'
+import "assets/styles/page404.scss"
 
-
-const PageNotFound:React.FC = (props) => {
+const PageNotFound: React.FC = (props) => {
     return (
         <Container className="App-container left">
 
-            <h2>PageNotFound</h2>
+            <section className="page_404">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-sm-12 ">
+                            <div className="col-sm-10 col-sm-offset-1  text-center">
+                                <div className="four_zero_four_bg">
+                                    <h1 className="text-center ">4<b>0</b>4</h1>
+
+
+                                </div>
+
+                                <div className="contant_box_404">
+                                    <h3 className="h2">
+                                        Look like you're lost
+                                    </h3>
+
+                                    <p>the page you are looking for not avaible!</p>
+
+                                    <Link to="/" className="link_404">Go to Home</Link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </Container>
     )
 }
